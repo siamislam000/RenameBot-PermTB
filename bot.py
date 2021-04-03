17094e27cfd3cdc2e44b12ca3de9d048f6edeade
siamislam000 <77488919+siamislam000@users.noreply.github.com>
2021-04-03 21:21:47 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import logging
+import logging -
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
